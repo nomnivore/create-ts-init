f2257f124b862ef610f6969c48bce9c963e84368
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2023-03-24 19:05:22 -0400

    enable strict

--- src/util/mergeObject.ts
+++ src/util/mergeObject.ts
@@ -1,19 +1,25 @@
-function isObject(obj: unknown): boolean {
-  return obj && typeof obj == "object" && !Array.isArray(obj);
+function isObject(obj: unknown): obj is object {
+  return obj !== null && typeof obj == "object" && !Array.isArray(obj);
 }
 
-export function mergeObject<T extends object, U extends object>(
-  target: T,
-  ...sources: U[]
-) {
-  if (!sources.length) return target;
+type MergedObject<T, U> = T & U;
+type MergeableObject = Record<string, unknown>;
+
+export function mergeObject<
+  T extends MergeableObject,
+  U extends MergeableObject
+>(target: T, ...sources: U[]): MergedObject<T, U> {
+  if (!sources.length) return target as T & U;
   const source = sources.shift();
 
   if (isObject(target) && isObject(source)) {
     for (const key in source as Record<keyof T & keyof U, unknown>) {
       if (isObject(source[key])) {
         if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeObject(target[key], source[key]);
+        mergeObject<T, MergeableObject>(
+          target[key] as T,
+          source[key] as MergeableObject
+        );
       } else {
         Object.assign(target, { [key]: source[key] });
       }
